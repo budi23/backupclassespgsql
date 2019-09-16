3f20abc473158bf6619b868d0b884e20302e6cc1
budi23 <30813262+budi23@users.noreply.github.com>
2019-09-16 15:03:17 +0700

    Add files via upload

---
+++ classes/database/Postgres94.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * PostgreSQL 9.4 support
+ *
+ */
+
+include_once('./classes/database/Postgres.php');
+
+class Postgres94 extends Postgres {
+
+	var $major_version = 9.4;
+
+	/**
+	 * Constructor
+	 * @param $conn The database connection
+	 */
+	function __construct($conn) {
+		$this->Postgres($conn);
+	}
+	function Postgres94($conn) {
+		$this->Postgres($conn);
+	}
+
+	// Help functions
+
+	function getHelpPages() {
+		include_once('./help/PostgresDoc94.php');
+		return $this->help_page;
+	}
+
+}
+?>
